d3e10d9e464975bd0d76014ad490332d9a49a0fc
Chandrkesh Kushwaha <ckushwaha@quizbee.co.in>
2022-04-30 17:13:57 +0530

    change code

--- api/controllers/users.controller.ts
+++ api/controllers/users.controller.ts
@@ -1,14 +1,14 @@
 import { usersService } from "../services";
 import { logInConsole } from "../utils/utils";
 
-export const getAllStoreData = async(request,response,next) => {
+export const getAllUserData = async(request,response,next) => {
     try{
 
-        const getAllStoreData = await storesService.getAllStoreByQuery({isActive:1});
-        return response.status(200).send({success:true, data:getAllStoreData});
+        const getAllUserData = await usersService.getAllUserByQuery({isActive:1});
+        return response.status(200).send({success:true, data:getAllUserData});
 
     } catch(err){
-        console.error('error in get all store data ->' ,err);
+        console.error('error in get all active users data ->' ,err);
         next(err);
     }
 }
